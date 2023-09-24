8223ca723f940e6c6e44fd7a2002d3a8d1ef4bf5
Sasha <sasha.zhulega.02@mail.ru>
2023-09-24 14:57:45 +0300

    optimaise questions

--- js/script.js
+++ js/script.js
@@ -10,10 +10,30 @@ const personalMovieDB  = {
     privatL: false
 };
 
+if(personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+
+
 for(let i = 0; i < 2; i++) {
-    const lastFilm = prompt('Один из последних просмотренных фильмов?', '');
-    const rating = prompt('На сколько оцените его?', '');
-    personalMovieDB.movies[lastFilm] = rating;
+    const lastFilm = prompt('Один из последних просмотренных фильмов?', ''),
+          rating = +prompt('На сколько оцените его?', '');
+
+    if(lastFilm != null && rating != null && lastFilm != '' && rating != '' && lastFilm.length < 50) {
+        personalMovieDB.movies[lastFilm] = rating;
+    } else {
+        i--;
+    }
+
 }
 
 console.log(personalMovieDB);
+
+
